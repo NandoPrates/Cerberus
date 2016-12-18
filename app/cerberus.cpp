62394a0143c3e78d482935ed3f93a22f0a74df80
Rafael Gorges <rafaelgorges@outlook.com>
2016-12-18 01:16:11 -0200

    Estrutura inicial

---
+++ app/cerberus.cpp
@@ -0,0 +1,9 @@
+/* Cerberus - password breaking method for linux etc/shadow, using brute force. 
+	@file cerberus.cpp
+	@author Protimus
+	@date 12/18/16
+*/
+
+#include "classes.h"
+
+int main(){}
---
+++ app/classes.h
@@ -0,0 +1,5 @@
+/* Cerberus - password breaking method for linux etc/shadow, using brute force. 
+@file classes.h
+@author Protimus
+@date 12/18/16
+*/
