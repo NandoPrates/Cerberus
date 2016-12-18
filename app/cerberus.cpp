44f7d21df193642cdeda86a5934cffb7d548a65b
Rafael Gorges <rafaelgorges@outlook.com>
2016-12-18 01:52:17 -0200

    Created a program menu.

--- app/cerberus.cpp
+++ app/cerberus.cpp
@@ -6,4 +6,31 @@
 
 #include "classes.h"
 
-int main(){}
+int main(){
+    int continuar=1;
+
+    do
+    {
+    	printf("\n\t======== Cerberus ========\n\n");
+    	printf("1. Cracking passwords\n");
+        printf("2. Check to /etc/shadow securty\n");
+        printf("3. Make /etc/shadow secure\n");
+        printf("0. Quit\n");
+
+        scanf("%d", &continuar);
+        system("cls || clear");
+        switch(continuar)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 0:
+                break;
+            default:
+                printf("Please provide a valid option!\n");
+        }
+    } while(continuar);
+}
